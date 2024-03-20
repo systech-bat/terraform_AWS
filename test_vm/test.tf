99a849775c3e97630b6c855e194b95fee7c4239f
systech-bat <EdwardArutyunyan@yandex.ru>
2024-03-20 23:46:14 +0400

    update

---
+++ test_vm/test.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "Test_server" {
+  ami                    = "ami-095413544ce52437d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.Test_server.id]
+  key_name = "oregon_key"
+  tags = {
+    Name  = "Test_server"
+    Owner = "someone"
+  }
+}
+
+resource "aws_security_group" "Test_server" {
+  name        = "Test Security Group"
+  description = "My SecurityGroup"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+
+  tags = {
+    Name  = "Test_Server SecurityGroup"
+    Owner = "someone"
+  }
+}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.Test_server.id
+}
--- webserver/webserver.tf
+++ webserver/webserver.tf
@@ -19,8 +19,9 @@ resource "aws_instance" "my_webs1" {
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform in AWS Cloud! my apache server works'"  >  /var/www/html/index.html
-echo "<br><front color="blue">hello world - this is a test web server" >> /var/www/html/index.html
+#echo "<h2>WebServer with IP: $myip</h2><br>каждый знает каждый гражданин кто хуйло #1'"  >  /var/www/html/index.html
+echo "<h2>WebServer with IP: каждый знает каждый гражданин кто хуйло #1'"  >  /var/www/html/index.html
+#echo "<br><front color="blue">hello world - this is a test web server" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
 EOF
