4e47bfbb1e7ea3b64279fafbcb68dceee2bbc11f
systech-bat <EdwardArutyunyan@yandex.ru>
2024-08-08 13:21:30 +0400

    s3 policy

--- s3/main.tf
+++ s3/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bazcorp_s3" {
-  count  = 2
-  bucket = "bazcorp-s3-0${count.index + 1}"
+  #count  = 2
+  bucket = "bazcorp-s3-01"
   acl    = "private"
 
   versioning {
@@ -25,15 +25,13 @@ resource "aws_s3_bucket" "bazcorp_s3" {
   }
 
   tags = {
-    Name        = "bazcorp-s3-0${count.index + 1}"
+    Name        = "bazcorp-s3-01"
     Environment = "Dev"
   }
 }
 
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  count  = length(aws_s3_bucket.bazcorp_s3)
-  bucket = aws_s3_bucket.bazcorp_s3[count.index].id
+  bucket = aws_s3_bucket.bazcorp_s3.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -42,3 +40,43 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "bazcorp_s3_pab" {
+  bucket = aws_s3_bucket.bazcorp_s3.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.bazcorp_s3.id
+
+  policy = <<-EOF
+    {
+      "Id": "SourceIP",
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "SourceIP",
+          "Action": "s3:*",
+          "Effect": "Deny",
+          "Resource": [
+            "arn:aws:s3:::bazcorp-s3-01",
+            "arn:aws:s3:::bazcorp-s3-01/*"
+          ],
+          "Condition": {
+            "NotIpAddress": {
+              "aws:SourceIp": [
+                "5.152.58.63"
+              ]
+            }
+          },
+          "Principal": "*"
+        }
+      ]
+    }
+    EOF
+}
+
