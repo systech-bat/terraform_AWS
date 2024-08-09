8981273b0d59a9576fd10e09870088c7954513d7
systech-bat <EdwardArutyunyan@yandex.ru>
2024-08-09 17:33:54 +0400

    s3 static web

--- s3/main.tf
+++ s3/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "bazcorp_s3" {
   #count  = 2
   bucket = "bazcorp-s3-01"
-  acl    = "private"
+  #acl    = "public-read"
 
   versioning {
     enabled = false
@@ -24,39 +24,47 @@ resource "aws_s3_bucket" "bazcorp_s3" {
     }
   }
 
+ server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     Name        = "bazcorp-s3-01"
     Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_acl" "bazcorp_s3" {
-    bucket = aws_s3_bucket.bazcorp_s3.id
-    acl    = "public-read"
-    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
-}
+#resource "aws_s3_bucket_acl" "bazcorp_s3" {
+#   bucket = aws_s3_bucket.bazcorp_s3.id
+#  acl    = "public-read"
+#    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+#}
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.bazcorp_s3.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.bazcorp_s3_pub]
-}
-
-
+#resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+#  bucket = aws_s3_bucket.bazcorp_s3.id
+#  rule {
+#    object_ownership = "BucketOwnerPreferred"
+#  }
+#  depends_on = [aws_s3_bucket_public_access_block.bazcorp_s3_pub]
+#}
 
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket = aws_s3_bucket.bazcorp_s3.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "arn:aws:kms:eu-central-1:562464429819:key/65d8b5bc-4019-4e79-945f-ccc2745d5614"
-    }
-  }
-}
+
+#resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+#  bucket = aws_s3_bucket.bazcorp_s3.id
+#
+#  rule {
+#    apply_server_side_encryption_by_default {
+#      sse_algorithm     = "aws:kms"
+#      kms_master_key_id = "arn:aws:kms:eu-central-1:562464429819:key/65d8b5bc-4019-4e79-945f-ccc2745d5614"
+#    }
+#  }
+#}
 
 resource "aws_s3_bucket_public_access_block" "bazcorp_s3_pub" {
   bucket = aws_s3_bucket.bazcorp_s3.id
@@ -72,13 +80,12 @@ resource "aws_s3_bucket_policy" "public_policy" {
 
   policy = <<-EOF
     {
-      "Id": "SourceIP",
       "Version": "2012-10-17",
       "Statement": [
         {
           "Sid": "SourceIP",
-          "Action": "s3:*",
           "Effect": "Deny",
+          "Action": "s3:*",
           "Resource": [
             "arn:aws:s3:::bazcorp-s3-01",
             "arn:aws:s3:::bazcorp-s3-01/*"
@@ -91,6 +98,16 @@ resource "aws_s3_bucket_policy" "public_policy" {
             }
           },
           "Principal": "*"
+        },
+        {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion"
+        ],
+          "Resource": "arn:aws:s3:::bazcorp-s3-01/*"
         }
       ]
     }
