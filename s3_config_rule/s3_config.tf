946d0dbdf430b1463974490122a44be7594a13ce
systech-bat <EdwardArutyunyan@yandex.ru>
2024-08-12 15:36:12 +0400

    s3_config_rules

---
+++ s3_config_rule/s3_config.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_config_config_rule" "s3_bucket_public_write_prohibited" {
+  name = "s3-bucket-public-write-prohibited"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_WRITE_PROHIBITED"
+  }
+}
+
+resource "aws_config_config_rule" "s3_bucket_server_side_encryption_enabled" {
+  name = "s3-bucket-server-side-encryption-enabled"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_SERVER_SIDE_ENCRYPTION_ENABLED"
+  }
+}
