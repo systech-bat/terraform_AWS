43dec8b6d23c512a2b39edbe331f72e2213b9153
systech-bat <EdwardArutyunyan@yandex.ru>
2024-08-19 11:45:54 +0400

    postgresql_secret_update

--- postgreSQL+secret/postgreSQL.tf
+++ postgreSQL+secret/postgreSQL.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 resource "aws_secretsmanager_secret" "db_secret02" {
   name = "postgres02_credentials"
+  tags = {
+    Name = "secret-for_postgreSQL"
+    Env  = "Dev"
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
@@ -34,15 +38,59 @@ resource "aws_db_instance" "postgresql" {
   publicly_accessible  = false
   multi_az             = false
 
-  # Enabling storage encryption using AWS Secrets Manager default key
   storage_encrypted = true
   kms_key_id        = "arn:aws:kms:eu-central-1:562464429819:key/36560e02-0ec0-4ded-a8d6-d17c9c87d385"
 
   skip_final_snapshot = true
 }
 
+resource "aws_secretsmanager_secret_rotation" "db_secret02_rotation" {
+  secret_id           = aws_secretsmanager_secret.db_secret02.id
+  rotation_lambda_arn = "arn:aws:lambda:eu-central-1:562464429819:function:SecretsManageraurora01_secret_rotation"
+
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "lambda_secretsmanager_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "secretsmanager:GetSecretValue",
+            "secretsmanager:DescribeSecret",
+            "secretsmanager:UpdateSecretVersionStage"
+          ]
+          Effect   = "Allow"
+          Resource = aws_secretsmanager_secret.db_secret02.arn
+        }
+      ]
+    })
+  }
+}
+
 variable "db_password" {
-  description = "The password for the PostgreSQL database"
+  description = "pass for pdb01"
   type        = string
   sensitive   = true
 }
